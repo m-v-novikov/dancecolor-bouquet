aa51def0092115147a2234f80bc4df9f474df551
m.v.novikov88 <m.v.novikov88@gmail.com>
2016-05-16 22:43:52 +0300

    set work space, header and banners with swiper

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        slim: {                              // Task
+            dist: {                            // Target
+                files: {                         // Dictionary of files
+                    './html/index.html': './slim/index.slim'
+                }
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {                         // Dictionary of files
+                    './css/main.css': './sass/index/main.scss'       // 'destination': 'source'
+                }
+            }
+        },
+        sprite:{
+            all:{
+                src: './files/images/sprites/**/**/**/*.png',
+                dest: './files/images/sprites.png',
+                destCss: './sass/generated/index/_sprites.scss',
+                cssTemplate: './grunt/templates/sprites.scss.handlebars',
+                imgPath: '../files/images/sprites.png',
+                padding: 2
+            }
+        },
+        watch: {
+            files: ['sass/**/*.scss', '**/**/*.slim', './files/images/sprites/**/**/**/*.png'],
+            tasks: ['slim', 'sass',  'sprite']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-slim');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-spritesmith');
+
+    grunt.registerTask('default', ['slim', 'sass', 'sprite', 'watch']);
+}
---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal'
+        //nextButton: '.swiper-button-next',
+        //prevButton: '.swiper-button-prev',
+    })
+});
